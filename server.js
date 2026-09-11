50c6010e2bda5857bce53093a6101f86f2391a1b
antondev23 <antonio.ortega0323@gmail.com>
2026-09-11 18:24:39 +0200

    produccion 3

--- server.js
+++ server.js
@@ -2,25 +2,16 @@ const express = require("express");
 const cors = require("cors");
 const fs = require("fs");
 const path = require("path");
-require("dotenv").config({ path: path.join(__dirname, ".env") });
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const allowedOrigins = [
-  process.env.APIFRONTEND_URL,
-  "http://localhost:8000",
-  "http://localhost:9000",
-].filter(Boolean);
 
 const corsOptions = {
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-      return;
-    }
-
-    callback(new Error("Origen no permitido por CORS"));
-  },
+  origin: [
+    process.env.APIFRONTEND_URL,
+    "http://localhost:8000",
+    "http://localhost:9000",
+  ],
   methods: ["GET"],
 };
 
